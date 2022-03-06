36ce760ade86aab8f40faa7ed24c0945e7fd820f
gyudoza <jujumilk3@gmail.com>
2022-03-06 16:25:30 +0900

    CLEAN - remove debug code

--- temp.py
+++ temp.py
@@ -2,20 +2,18 @@ import re
 import requests
 
 from bs4 import BeautifulSoup
+from requests.exceptions import ConnectionError
 
 
 def is_spam(content, spam_domains, depth):
     if 'http' not in content:
         return False
     session = requests.Session()
-    print(content, spam_domains, depth)
     found_addresses = re.findall(r'(https?://\S+)', content)
-    print(found_addresses)
     for address in found_addresses:
         while depth:
             depth -= 1
             response = session.head(address, allow_redirects=False)
-            print(response.content)
             if response.status_code in [301, 302]:
                 address = response.headers['Location']
             else:
@@ -23,7 +21,6 @@ def is_spam(content, spam_domains, depth):
                 found_link = bs4_content.find('a')
                 if found_link.has_attr('href'):
                     address = found_link['href']
-        print("final address is:", address)
         bs4_content = None
         found_links = None
         for spam_domain in spam_domains:
@@ -31,25 +28,38 @@ def is_spam(content, spam_domains, depth):
                 return True
             else:
                 if bs4_content is None:
-                    bs4_content = BeautifulSoup(requests.get(address).content, 'html.parser')
+                    try:
+                        get_response = requests.get(address)
+                    except ConnectionError:
+                        continue
+                    bs4_content = BeautifulSoup(get_response.content, 'html.parser')
                     found_links = bs4_content.find_all('a')
                 for found_link in found_links:
                     if found_link.has_attr('href'):
                         found_address = found_link['href']
                         if spam_domain in found_address:
                             return True
-                        print(found_address)
-    print('----------------------------------------')
-    print()
     return False
 
-# spam
+
+# spam cases
+# depth1 spam
 assert is_spam('asdw https://goo.gl/88srfV', ['pearsoncmg.com'], 1)
-# no spam
-assert is_spam('asdw https://goo.gl/88srfV', ['nospam'], 1) == False
-# find from html
+# depth2 spam
+assert is_spam('asdw https://goo.gl/88srfV', ['support'], 2)
+# depth3 spam
+assert is_spam('asdw https://goo.gl/88srfV', ['pearson'], 3)
+# find from html element
 assert is_spam('asdw https://goo.gl/88srfV', ['accessibility'], 1)
 # many addresses at content
 assert is_spam('asdw https://goo.gl/88srfV http://bit.ly/silly', ['pearsoncmg.com'], 1)
 
-
+# no spam cases
+# depth1 no spam
+assert is_spam('asdw https://goo.gl/88srfV', ['nospam'], 1) == False
+# depth2 no spam
+assert is_spam('asdw https://goo.gl/88srfV', ['nospam'], 2) == False
+# many addresses and expired redirection
+assert is_spam('asdw https://goo.gl/88srfV http://bit.ly/silly', ['pass', 'obvsy.com'], 1) == False
+# many addresses and depth
+assert is_spam('asdw https://goo.gl/88srfV http://bit.ly/silly', ['pass', 'obvsy.com'], 2) == False
