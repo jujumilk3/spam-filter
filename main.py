7d266ae8e62dc38d814129f6dad8e49237df3e42
gyudoza <jujumilk3@gmail.com>
2022-03-06 16:34:55 +0900

    FEATURE - spam_filter_endpoint

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
+from typing import List
+from pydantic import BaseModel
+
+from utils import is_spam
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class SpamFilter(BaseModel):
+    content: str
+    spam_domains: List[str]
+    depth: int
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/spam-filter")
+async def spam_filter(
+    request_body: SpamFilter
+):
+    return is_spam(**request_body.dict())
