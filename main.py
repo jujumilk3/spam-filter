c53514a9847a41f3855a1164c2f7e8df13c7b1d4
gyudoza <jujumilk3@gmail.com>
2022-03-06 15:50:49 +0900

    CLEAN - remove unusing endpoints and pkg

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
@@ -7,16 +6,3 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-# Samples
-
-@app.get("/redirect", response_class=RedirectResponse)
-async def redirect():
-    return "http://localhost:8000/docs"
-
-
-@app.get("/reredirect", response_class=RedirectResponse)
-async def reredirect():
-    return "http://localhost:8000/redirect"
-
